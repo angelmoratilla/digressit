90c39db1f1488068fadcc1cf73eb7405dcbb2893
rebecca.younes <rebecca.younes@b80beb6a-a484-11de-b98f-4500b42dc123>
2012-02-02 15:52:03 +0000

    Tag regulationroom-rel-5.14.0

--- themes/digressit-wireframe/search-template.php
+++ themes/digressit-wireframe/search-template.php
@@ -1,15 +1,12 @@
 <?php
-/**
- * @package Digressit
- * @subpackage Digressit_Wireframe
- * Template Name: Search
+/*
+Template Name: Search
 */
 ?>
 <?php get_header(); ?>
 <div id="primary-wrapper">
 	<div id="primary" role="main">
 		<div id="notices"></div>
-		<a name="startcontent" id="startcontent"></a>
 		<div id="current-content" class="hfeed">
 			<h2><?php _('Search'); ?></h2>
 			<div id="search"><h4><?php _e('Search'); ?></h4>
